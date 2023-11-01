5afac6b9877217be22018c4e8db2c61b6d950b19
Juan Pablo Giraldo Atehortua <juanpaj542@gmail.com>
2023-11-01 07:45:37 -0500

    cambios

---
+++ operacionesGIT/trabajo.js
@@ -0,0 +1,6 @@
+var numero1 = parseInt(prompt("Ingrese Un numero"));
+var numero2 = parseInt(prompt("Ingrese otro numero"));
+
+var total = numero1 + numero2
+
+document.write("El resultado es: ", total)
